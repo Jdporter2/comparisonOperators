ea5978f366e70b56709e30abed98231c66850d2c
Jdporter2 <55327114+Jdporter2@users.noreply.github.com>
2019-10-05 11:49:28 -0500

    Initial commit

---
+++ practiceComparisonOperators.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+
+''' ==   Checks if two values are equal '''
+print(4==4)
+print(4==2)
+
+''' > or <   Checks if two values are greater or less than each other '''
+print(2>3)
+print(3>2)
+
+''' => =<   Checks if two values are greater/equal to or less/ equal to each other '''
+print(4>=4)
+print(5>=4)
+print(4<=4)
+print(5<=4)
+
+harry = (4==5)
+print("Result: " + str(harry))
